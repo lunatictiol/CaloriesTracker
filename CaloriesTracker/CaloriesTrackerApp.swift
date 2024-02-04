31b6e747de5d79aac3d05d9150d30200936c3918
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-04 15:50:57 +0530

    Initial Commit

---
+++ CaloriesTracker/CaloriesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesTrackerApp.swift
+//  CaloriesTracker
+//
+//  Created by sabiq on 04/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
